445ffedffb03e95a41f152e7cdd2f636f91e650f
kanekos99 <kanekos99@users.noreply.github.com>
2026-08-10 13:35:53 +0000

    deploy: 516719d8c38fe2e353bd066706919b43dcaf4ef3

---
+++ scripts/archive-gallery.js
@@ -0,0 +1,33 @@
+// const gallery = document.getElementById("archive-gallery");
+
+// const galleryImage = [
+//     "{{ '/assets/archive/1.png' | url }}",
+//     "{{ '/assets/archive/2.png' | url }}",
+//     "{{ '/assets/archive/3.png' | url }}",
+//     "{{ '/assets/archive/4.png' | url }}",
+//     "{{ '/assets/archive/5.png' | url }}",
+//     "{{ '/assets/archive/6.png' | url }}",
+//     "{{ '/assets/archive/7.png' | url }}",
+//     "{{ '/assets/archive/8.png' | url }}",
+//     "{{ '/assets/archive/9.png' | url }}",
+//     "{{ '/assets/archive/10.png' | url }}",
+//     "{{ '/assets/archive/11.png' | url }}",
+//     "{{ '/assets/archive/12.png' | url }}",
+//     "{{ '/assets/archive/13.png' | url }}",
+//     "{{ '/assets/archive/14.png' | url }}",
+//     "{{ '/assets/archive/15.png' | url }}",
+//     "{{ '/assets/archive/16.png' | url }}",
+// ]
+
+// galleryImage.forEach((image) => {
+//     const imageHtml = `
+//       <img src="${image}" 
+//                  class="gallery-thumbnail img-fluid gallery-img" 
+//                  loading="lazy"         
+//                  onclick="showImage(this);" 
+//                  data-bs-toggle="modal"
+//                  data-bs-target="#galleryModal"
+//         />
+//     `; 
+//     gallery.innerHTML += imageHtml;
+// })
